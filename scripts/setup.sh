5ca806c3c96b20d109857647da3e2bd82646b054
susagi <susagi@negitorodon.de>
2025-04-15 16:30:51 +0200

    feature/setup-script: script renamed, now usable for reproducible workflows

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -20,7 +20,7 @@ sudo apt-get -y install git vim || error_exit "Installation of git and vim faile
 echo "Enabling 1-Wire interface..."
 sudo raspi-config nonint do_onewire 0 || error_exit "Enabling the 1-Wire interface failed." 
 
-# Configure GPIO for 1-Wire
+# Configure GPIO for 1-Wire on GPIO Pin 2
 echo "Configuring GPIO for 1-Wire on GPIO2..."
 sudo cp /boot/firmware/config.txt /boot/firmware/config_backup.txt
 sudo sed -i '/^dtoverlay=w1-gpio/ s/.*/dtoverlay=w1-gpio,gpiopin=2/' /boot/firmware/config.txt || echo "dtoverlay=w1-gpio,gpiopin=2" | sudo tee -a /boot/firmware/config.txt
@@ -31,7 +31,7 @@ echo "Loading 1-Wire kernel modules..."
 sudo modprobe w1-gpio || error_exit "Loading w1-gpio module failed."
 sudo modprobe w1-therm || error_exit "Loading w1-therm module failed." 
 
-# Verify 1-Wire Sensor 
+# Verify 1-Wire Sensor ###not working because reboot is needed. Maybe wen can use this snippet somehow else
 #
 #echo "Verifying 1-Wire sensor..."
 #SENSOR_BASE="/sys/bus/w1/devices" 
@@ -51,11 +51,4 @@ sudo modprobe w1-therm || error_exit "Loading w1-therm module failed."
 #fi
 #shopt -u nullglob
 echo "1-Wire configuration completed." 
-
-
-# Edit Raspberry Pi Configuration File 
-
-#echo "Opening Raspberry Pi configuration file for editing..."
-#sudo nano /boot/firmware/config.txt 
-
 echo "Script completed successfully. Please reboot." 
